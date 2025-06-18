6393f2c396011ceff39a04cc1a85f4671d8796f7
Your Name <you@example.com>
2025-06-18 14:24:38 +0300

    Uploading Files and Validation

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,5 +1,7 @@
 from helpers.config import Settings, get_settings
 import os
+import random
+import string
 
 
 class BaseController:
@@ -11,3 +13,6 @@ class BaseController:
             self.base_dir,
             "assets/files"
         )
+
+    def generate_random_string(self, length: int = 12):
+        return ''.join(random.choices(string.ascii_lowercase+string.digits, k=length))
--- src/routes/data.py
+++ src/routes/data.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import Settings, get_settings
 from controllers import DataController, ProjectController
+from models import ResponseSignal
 import os
+import aiofiles
+import logging
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -13,8 +18,8 @@ data_router = APIRouter(
 @data_router.post("/upload/{project_id}")
 async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
-    # Validate the file properties
-    is_valid, result = DataController().validat_uploaded_file(file=file)
+    # Validate the file
+    is_valid, result = DataController().validate_uploaded_file(file=file)
 
     if not is_valid:
         return JSONResponse(
@@ -24,8 +29,24 @@ async def upload_data(project_id: str, file: UploadFile,
             }
         )
 
-    project_file_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = os.path.join(
-        project_file_path,
-        file.filename
+    file_path = DataController().generate_unique_filename(
+        original_file_name=file.filename,
+        project_id=project_id
+    )
+    try:
+        async with aiofiles.open(file_path, mode='wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        logger.error(f'Error while uploading file: {e}')
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
+    return JSONResponse(
+        content={
+            "signal": result
+        }
     )
