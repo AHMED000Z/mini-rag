3772ef836c3be7a191fa58872201d434dd7fe7a3
AHMED000Z <ahmed.ibrahim201466yahoo@gmail.com>
2025-08-24 19:28:48 +0300

    Implement file upload functionality with validation and project directory management

---
+++ src/controller/DataController.py
@@ -0,0 +1,16 @@
+from .BaseController import BaseController
+from fastapi import UploadFile
+from model import ResponseSignal
+class DataController(BaseController):
+    def __init__(self):
+        super().__init__()
+
+    def validat_uploaded_file(self,file:UploadFile):
+        self.size_scale=1048576
+        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED
+        
+        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED
+        
+        return True, ResponseSignal.FILE_UPLOAD_SUCCESS
